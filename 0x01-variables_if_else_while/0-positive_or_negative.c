fb6d678edf9d32dc251dcd7d330c17c8cf67f63b
Shile <adelove4u@gmail.com>
2022-09-14 17:50:42 -0700

    Task 0-9

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,9 +9,8 @@
  *
  * Description: using the main function
  *
- * this program prints "Programming is positive, zero, or negative
- *
- * Return: 0*
+ * this program prints "Programming is positive, zero, or negative"
+ * Return: 0
  */
 
 int main(void)
